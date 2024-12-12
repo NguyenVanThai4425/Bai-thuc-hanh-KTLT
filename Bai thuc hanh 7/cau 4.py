6382891a6b45115238ada87ad52cc4fe23b72cbb
NguyenVanThai4425 <thaivnguyen1234@gmail.com>
2024-12-12 08:18:02 +0700

    Update cau 4.py

--- Bai thuc hanh 7/cau 4.py
+++ Bai thuc hanh 7/cau 4.py
@@ -4,4 +4,4 @@ def file_read_from_head(fname,nline):
     with open (fname) as f:
         for line in islice(f,nline):
             print(line)
-file_read_from_head('C:/Users/DELL Inc/OneDrive - vinhuni.edu.vn/Tài liệu/vd.txt',2)
+file_read_from_head('vd.txt',2)
